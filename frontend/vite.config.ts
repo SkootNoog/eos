a7dc0b0a99bd184e912820b2ae966bb09f6543e4
SkootNoog <scottlnegus@gmail.com>
2024-12-31 15:32:54 -0600

    dockerize frontend, backend, and database

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,6 +10,13 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  server: {
+    host: "0.0.0.0",
+    port: 8080,
+    watch: {
+      usePolling: true,
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
